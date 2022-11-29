34070c317d1af31cbda7438e89a627a44578468f
skz <zafar.syedkashif@gmail.com>
2022-11-29 22:54:09 +0530

    layout

--- jscript.js
+++ jscript.js
@@ -1,9 +1,16 @@
 const l_arrt=['Home','Profile','Messages','History','Tasks','Communities']
 const l_arrb=['Settings','Support','Privacy']
 const l_bar=document.querySelector('#l_bar')
+const logo_div=document.createElement('div')
+const logo_div2=document.createElement('div')
 const logo=document.createElement('div')
-logo.id='logo'
+logo_div.id='logo'
 logo.innerHTML='Logo'
+const logo_img=document.createElement('img')
+logo_img.alt='logo';
+logo_div2.appendChild(logo_img)
+logo_div2.appendChild(logo)
+logo_div.appendChild(logo_div2)
 const bart=document.createElement('div')
 bart.id='bart'
 for(var i=0;i<l_arrt.length;i++){
@@ -32,6 +39,6 @@ for(var i=0;i<l_arrb.length;i++){
     bar_div.appendChild(bar_text)
     barb.appendChild(bar_div)
 }
-l_bar.appendChild(logo)
+l_bar.appendChild(logo_div)
 l_bar.appendChild(bart)
 l_bar.appendChild(barb)
